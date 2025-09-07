b140e31f85d0dc8a3507e9d9553e42efbb44753e
Godstime Pious <godstimepious33@gmail.com>
2025-09-07 11:52:12 +0100

    Implemented getTaskById endpoint

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -23,3 +23,17 @@ export const getAllTasks = async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 };
+
+export const getTaskById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const task = await Todo.find({ _id: id, user: req.user.id });
+    console.log(task);
+    if (task.length === 0) {
+      return res.status(404).json({ message: `Task ${id} not found` });
+    }
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(404).json({ error: `Task ${id} not found` });
+  }
+};
--- src/routes/Task.js
+++ src/routes/Task.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middleware.js";
-import { createTask, getAllTasks } from "../controllers/task.controller.js";
+import {
+  createTask,
+  getAllTasks,
+  getTaskById,
+} from "../controllers/task.controller.js";
 
 const taskRouter = Router();
 
 taskRouter.post("/create", auth, createTask);
 taskRouter.get("/", auth, getAllTasks);
+taskRouter.get("/:id", auth, getTaskById);
 export default taskRouter;
